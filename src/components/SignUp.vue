ddbc36f9fd84713e048343240b82ce44032228f5
dilmiishara2002 <dilmiishara2000@gmail.com>
2025-03-12 00:18:10 +0530

    signup API

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,17 +1,45 @@
 <template>
     <h1>Sign Up</h1>
     <div class="register">
-        <input type="text" placeholder="Enter name">
-        <input type="text" placeholder="Enter Email">
-        <input type="password" placeholder="Enter Password">
-        <button>Sign Up</button>
+        <input type="text" v-model="name" placeholder="Enter name">
+        <input type="text" v-model="email" placeholder="Enter Email">
+        <input type="password" v-model="password" placeholder="Enter Password">
+        <button v-on:click="signUp">Sign Up</button>
     </div>
 </template>
 
 <script>
 
+import axios from 'axios';
+
 export default{
-    name: 'SignUp'
+    name: 'SignUp',
+    data(){
+        return{
+            name:'',
+            email:'',
+            password:''
+        }
+        
+    },
+    methods:{
+        async signUp(){
+            console.warn("Sign up",this.name,this.email,this.password)
+            let result = await axios.post("http://localhost:3000/user",{
+                email:this.email,
+                password:this.password,
+                name:this.name
+            })
+
+            console.warn(result);
+
+            if(result.status==201)
+            {
+                alert("sign-up done");
+                localStorage.setItem("user-info",JSON.stringify(result.data))
+            }
+        }
+    }
 }
 </script>
 
