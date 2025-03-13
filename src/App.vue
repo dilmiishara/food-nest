def575f9494bd77f59881cf089f386601960f0af
dilmiishara2002 <dilmiishara2000@gmail.com>
2025-03-13 22:45:55 +0530

    API for Add resturant

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,7 @@ export default {
     width: 100px;
 }
 
-.register input{
+.register input, .login input, .add input{
     width: 300px;
     height: 40px;
     padding-left: 20px;
@@ -34,7 +34,7 @@ export default {
     border: 1px solid skyblue;
 }
 
-.register button{
+.register button, .login button, .add button{
     width: 320px;
     height: 40px;
     border: 1px solid skyblue;
--- src/components/AddRest.vue
+++ src/components/AddRest.vue
@@ -1,15 +1,51 @@
 <template>
     <HeaderCom/>
     <h1>Hello User, Welcome on Add restaurant Page </h1>
+    <form class="add">
+      <input type="text" name="name" placeholder="Enter Name" v-model="resturant.name"/>
+      <input type="text" name="address" placeholder="Enter Address" v-model="resturant.address"/>
+      <input type="text" name="contact" placeholder="Enter Contact" v-model="resturant.contact"/>
+      <button type="button" v-on:click="addRest">Add new resturant</button>
+    </form>
 </template>
 
 <script>
 import HeaderCom from './HeaderCom.vue'
+import axios from 'axios'
 
 export default {
   name: "AddRest", 
   components:{
     HeaderCom
+  },
+
+  data(){
+    return{
+      resturant:{
+        name:'',
+        address:'',
+        contact:''
+      }
+    }
+  },
+
+  methods:{
+    async addRest(){
+      console.log(this.resturant)
+      const result=await axios.post("http://localhost:3000/resturant",{
+        name:this.resturant.name,
+        address:this.resturant.address,
+        contact:this.resturant.contact
+      });
+
+      if(result.status ==201){
+        this.$router.push({name:'HomePage'})
+      }
+      console.warn("result",result)
+
+    }
+
+
   },
 
   mounted(){
