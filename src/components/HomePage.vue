843fbfb3b4e0933cb6d923439f4a96d99080ab35
dilmiishara2002 <dilmiishara2000@gmail.com>
2025-03-13 20:25:16 +0530

    list of resturant with API

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,16 +1,34 @@
 <template>
     <HeaderCom/>
     <h1>Hello {{name}}, Welcome on Home Page </h1>
+    <div>
+        <table border="1">
+            <tr>
+                <!-- <th>ID</th> -->
+                <th>Name</th>
+                <th>Contact</th>
+                <th>Address</th>
+            </tr>
+            <tr v-for="item in resturant" :key="item.id">
+                <!-- <td>{{item.id}}</td> -->
+                <td>{{item.name}}</td>
+                <td>{{item.contact}}</td>
+                <td>{{item.address}}</td>
+            </tr>
+        </table>
+    </div>
 </template>
 
 <script>
 import HeaderCom from './HeaderCom.vue'
+import axios from 'axios'
 
 export default {
   name: "HomePage", 
   data() {
     return { 
-      name: ''
+      name: '',
+      resturant:[]
     };
   },
 
@@ -18,12 +36,24 @@ export default {
     HeaderCom
   },
 
-  mounted(){
+  async mounted(){
     let user = localStorage.getItem('user-info');
     this.name = JSON.parse(user).name;
     if(!user){
         this.$router.push({name:'SignUp'})
     }
+
+    let result =await axios.get("http://localhost:3000/resturant");
+    console.warn(result);
+    this.resturant = result.data;
+
 }
 };
 </script>
+
+<style>
+table{
+    height:auto;
+    width:75%;
+}
+</style>
