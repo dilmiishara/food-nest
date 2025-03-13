19c33672033046d303ff0e839e0bddf0ad2b182c
dilmiishara2002 <dilmiishara2000@gmail.com>
2025-03-13 15:33:36 +0530

    Header

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,11 +1,16 @@
 <template>
+    <HeaderCom/>
     <h1>Hello User, Welcome on Home Page </h1>
 </template>
 
 <script>
+import HeaderCom from './HeaderCom.vue'
 
 export default {
   name: "HomePage", 
+  components:{
+    HeaderCom
+  },
 
   mounted(){
     let user = localStorage.getItem('user-info');
